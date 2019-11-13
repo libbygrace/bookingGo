36fba0c20b4eca5e749c2b4bc94c0ebf65cb0ffb
Libby Grace <libby.grace@intouchnetworks.com>
2019-11-13 13:41:42 +0000

    Created A Helper To Convert The Place Type

---
+++ src/helpers/search.js
@@ -0,0 +1,15 @@
+export const placeTypeConversion = placeType => {
+    switch( placeType ){
+        case 'A':
+            return 'Airport';
+        case 'C':
+            return 'City';
+        case 'T':
+            return 'Station';
+        case 'Z':
+        case 'G':
+            return 'Place';
+        default:
+            return null;
+    }
+};
