4f062c6045f9b0c0c9daade63d1624767e834e25
Libby Grace <libby.grace@intouchnetworks.com>
2019-11-13 13:42:11 +0000

    Hooked The Store Into The App

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store';
+
 import './index.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import Search from './search/search';
 import * as serviceWorker from './serviceWorker';
 
 const routing = (
-    <Router>
-        <div>
-            <Route exact path="/" component={ App } />
-            <Route path="/search" component={ Search } />
-        </div>
-    </Router>
-)
+    <Provider store={configureStore()}>
+        <Router>
+            <div>
+                <Route exact path="/" component={ App } />
+                <Route path="/search" component={ Search } />
+            </div>
+        </Router>
+    </Provider>
+);
 ReactDOM.render( routing, document.getElementById( 'root' ) );
 
 // If you want your app to work offline and load faster, you can change
